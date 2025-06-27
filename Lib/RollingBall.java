8f63c5ad11f74a2cc324056bc3f3b266235cdf18
Podsawee123 <podsawee.p@ku.th>
2025-06-27 15:53:42 +0700

    Add files via upload

--- Lib/RollingBall.java
+++ Lib/RollingBall.java
@@ -1,1 +1,18 @@
+package Lib;
+import java.util.Timer;
+import java.sql.Time;
+import java.util.Timer.*;
+import java.awt.Graphics;
+import java.awt.event.*;
+import javax.swing.Action;
+import javax.swing.JPanel;
 
+public class RollingBall extends JPanel {
+   
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawOval(100,90,80,80);
+        g.fillArc(100,90,80,80,0,180);
+    }
+  
+}
